8390752f9dc56d859954d6fc8b60a758bf047185
JustinBrown <brown.jt@gmail.com>
2014-07-15 02:45:57 -0600

    A whole ton of shit: cleaned up ham-d, added a Projects page, implemented description overlay for projects, applied grid to About page, corrected footer overlap,... and probably a bunch of other stuff I can't remember

--- js/blog.js
+++ js/blog.js
@@ -12,25 +12,54 @@
 // Mobile Navigation
 $('.mobile-toggle').click(function() {
     console.log("script loading?");
-    if ($('.site-header').hasClass('open-nav')) {
-        $('.site-header').removeClass('open-nav');
+    if ($('.site-header').hasClass('nav-overlay')) {
+        $('.site-header').removeClass('nav-overlay');
     } else {
-        $('.site-header').addClass('open-nav');
+        $('.site-header').addClass('nav-overlay');
     };
-    if ($('.site-nav').hasClass('is-closed')) {
-        // console.log("site-nav is closed");
-        $('.site-nav').removeClass('is-closed');
-        $('.site-nav').addClass('is-open');
+    if ($('.nav-links').hasClass('is-closed-for-mobile')) {
+        // console.log("nav-links is closed");
+        $('.nav-links').removeClass('is-closed-for-mobile');
+        $('.nav-links').addClass('is-open-for-mobile');
     } else {
-        $('.site-nav').removeClass('is-open');
-        $('.site-nav').addClass('is-closed');
+        $('.nav-links').removeClass('is-open-for-mobile');
+        $('.nav-links').addClass('is-closed-for-mobile');
     };
 });
 
+// Project Shots
+// $(.project-overlay).css('height',$(.project-shot).innerHeight());
+var currentHeight = 0;
+var currentWidth = 0;
+$(window).load(function() {
+    //get the natural page height -set it in variable above.
+
+    // // Pseudo Code
+    // For each project-shot
+    //     get the child img height
+    //     set the child overlay height to match
+
+    $('.project-shot').each(function() {
+        currentHeight = $('img', this).outerHeight();
+        console.log("currentHeight = " + currentHeight);
+        $('.project-overlay', this).css('height', currentHeight);
+
+    })
+
+    // $('.project-overlay').each(function() {
+    //     currentHeight = $('.project-shot img').outerHeight();
+    //     console.log("currentHeight = " + currentHeight);
+    //     $(this).css('height', currentHeight);    
+    // });
+    currentWidth = $('.project-shot img').outerWidth();
+    $('.project-overlay').css('width', currentWidth);
+
+});
+
 // $('.site-header li a').click(function() {
-//     if ($('.site-header').hasClass('open-nav')) {
-//         $('.navigation').removeClass('open-nav');
-//         $('.site-header').removeClass('open-nav');
+//     if ($('.site-header').hasClass('nav-overlay')) {
+//         $('.navigation').removeClass('nav-overlay');
+//         $('.site-header').removeClass('nav-overlay');
 //     }
 // });
 
