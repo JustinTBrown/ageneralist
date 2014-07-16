f89eebf948a2d434c3ae84138f8f0e24f12324ff
JustinBrown <brown.jt@gmail.com>
2014-07-16 11:33:04 -0600

    Cleaned up default.html and implemented a JS require tree with Require.JS. Removed script tags from default.html and moved font links to stylesheets/base/fonts.

---
+++ js/main.js
@@ -0,0 +1,6 @@
+require(["main/jquery-1.11.1.min"], function(util) {});
+require(["main/blog"], function(util) {});
+require(["main/highlight.pack"], function(util) {
+	hljs.initHighlightingOnLoad();
+});
+// hljs.initHighlightingOnLoad();
---
+++ js/main/live-reload.js
@@ -0,0 +1,3 @@
+// Live Reload
+// Connects browser automatically to local instance
+document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
