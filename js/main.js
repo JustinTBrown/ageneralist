4f736c5b134ecb6b07e3ab2f45c637c9b7457cc7
JustinBrown <brown.jt@gmail.com>
2014-09-05 15:57:54 -0600

    RequireJS pipeline fixed. Mobile ham-d now working.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // require(["main/jquery-1.11.1.min"], function(util) {});
 
-// require(["jquery"], function() {
-// 	console.log("jquery loaded");
-// });
-// require(["main/blog"], function() {
-// 	console.log("blog.js loaded");
-// });
+require(["jquery"], function() {
+	console.log("jquery loaded");
+});
+require(["main/blog"], function() {
+	console.log("blog.js loaded");
+});
 require(["main/highlight.pack"], function() {
 	hljs.initHighlightingOnLoad();
 });
