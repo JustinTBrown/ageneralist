16eae6101ad3d510de3001fdc4ebed06625a747e
JustinBrown <brown.jt@gmail.com>
2014-10-18 15:14:57 -0600

    Adding tiles for Recent Posts in index.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@
 require(["jquery"], function() {
 	console.log("jquery loaded");
 });
-require(["main/blog"], function() {
-	console.log("blog.js loaded");
-});
 require(["main/highlight.pack"], function() {
 	hljs.initHighlightingOnLoad();
 	console.log("hljs loaded");
+});
+require(["main/blog"], function() {
+	console.log("blog.js loaded");
 });
 require(["main/google-analytics"], function() {});
